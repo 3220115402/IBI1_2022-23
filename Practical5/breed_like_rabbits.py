74ef93b155df0166ff27c19d04f05f0e6d92e3a4
闫如 <yanyanru@yanyanrudeMacBook-Air.local>
2023-03-17 17:16:12 +0800

    week 5 task

---
+++ Practical5/breed_like_rabbits.py
@@ -0,0 +1,11 @@
+#At the beginning we hve 2 rabbits,each pair born 2 rabbits,total rabbits number equals the former total number plus the new born rabbits
+#at the beginning there are two rabbits
+total=2
+#this is the first generation
+generation=1
+#loop for generation 
+while total<100:
+  newborn=total/2*2
+  total+=newborn
+  generation+=1
+print("The total number is over 100 after generation",generation)
---
+++ Practical5/variables.py
@@ -0,0 +1,20 @@
+a=-3.19
+b=-118.24
+c=116.39
+d=a-b
+e=c-a
+if d>e:
+    print("Los Angeles is further")
+elif d==e:
+    print("Los Angeles is the same as Haining")
+else:
+    print("Haining is further")
+#e is greater,Haining is further
+X=1+1==2
+Y=1+2==4
+print(X,Y)
+#True False
+Z=X and Y
+W=X or Y
+print(Z,W)
+#False True
